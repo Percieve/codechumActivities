cd66276a3a3832df982eeb1cc045e4f713b5ac4e
Percieve <111714672+Percieve@users.noreply.github.com>
2024-03-13 11:38:45 +0700

    Create AccountAssessment.cs
    creating an account assessment base for LAN Account System Model.

---
+++ AccountAssessment.cs
@@ -0,0 +1,17 @@
+public abstract class custom_methods
+{
+  public double compute_for();
+  public void assess_results();
+}
+
+public class execute_methods
+{
+  private int method1;
+  private double method2;
+}
+
+public class confirm_methods
+{
+  private bool isVerified;
+  private int numofVerified;
+}
